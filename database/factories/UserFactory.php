c5f501fb304176c73c71dd2e4b90cf19fab7e17f
mohamed <zayanimed97@gmail>
2022-02-21 03:14:01 +0100

    User seeder and factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
@@ -15,11 +14,16 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'uuid' => $this->faker->uuid(),
+            'first_name' => $this->faker->name(),
+            'last_name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'is_admin' => 0,
+            'password' => bcrypt('userpassword'), // password
+            'avatar' => null,
+            'address' => $this->faker->address(),
+            'phone_number' => $this->faker->phoneNumber(),
+            'is_marketing' => rand(0,1),
         ];
     }
 
