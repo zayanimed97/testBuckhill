b406cbf58f242dc01702d14d89cf544a3a6c293e
mohamed <zayanimed97@gmail>
2022-02-21 03:30:47 +0100

    category new factory and update seeder

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
-use Faker\Factory as Faker;
 
 class CategoriesSeeder extends Seeder
 {
@@ -16,15 +14,6 @@ class CategoriesSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create();
-        $amaount = random_int(1, 10);
-        for ($i=0; $i < $amaount; $i++) { 
-            $title = $faker->text(50);
-            $category = new Category();
-                $category->uuid = (string) Str::uuid();
-                $category->title = $title;
-                $category->slug = Str::slug($title);
-            $category->save();
-        }
+        Category::factory()->count(rand(10,15))->create();
     }
 }
